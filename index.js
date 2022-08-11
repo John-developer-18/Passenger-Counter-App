53e6b50bc1a93356aaa2367694139b4edf75ccfa
John <falanajohn6@gmail.com>
2022-08-11 23:54:40 +0100

    Finally got the sum to work

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 
-let counter = document.getElementById("counter")
-let saver = document.getElementById("save-str")
+let counter = document.getElementById("counter");
+let saver = document.getElementById("save-str");
+let sumr = document.getElementById("sum-str");
 let count = 0;
+let saveArr=[];
 
 function increment(){
     count++;
@@ -19,8 +21,20 @@ function decrement(){
 }
 
 function save(){
-    let save=count + " - ";
+    saveArr.push(count);
+    
+    
+    let sum=0;
 
+    for (i=0; i<saveArr.length; i++){
+        sum+=saveArr[i];
+    }
+      sumr.textContent+=sum+" "+"-";
+        console.log(saveArr);
+      
+      
+    let save=count + " - ";
+ 
     saver.textContent+=save;
 
     count=0;
